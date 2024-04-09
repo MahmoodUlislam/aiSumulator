17f2d5ca5faed11d9d6cfd49b754dee0b4bd1f73
Mahmood <mahmood.islam@gmail.com>
2024-04-09 17:34:09 -0600

    test deployment

--- src/pages/loading.js
+++ src/pages/loading.js
@@ -1,12 +1,12 @@
-import styles from "./loading.module.css";
-import Image from "next/image";
+// import styles from "./loading.module.css";
+// import Image from "next/image";
 
-const Loading = () => {
-  return (
-    <div>
-      <Image src="/ai-loading.gif" styles={styles.aiLoading} alt="loading..." />
-    </div>
-  );
-};
+// const Loading = () => {
+//   return (
+//     <div>
+//       <Image src="/ai-loading.gif" styles={styles.aiLoading} alt="loading..." />
+//     </div>
+//   );
+// };
 
-export default Loading;
+// export default Loading;

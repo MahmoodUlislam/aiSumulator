207df499dbd75500bc66c1fc535da1e3b0dbbeed
Mahmood <mahmood.islam@gmail.com>
2024-04-11 16:36:13 -0600

    conditional rendering in the confirmation page

--- src/pages/confirmation.js
+++ src/pages/confirmation.js
@@ -1,53 +1,54 @@
 "use client";
 
-import Link from "next/link";
 import Image from "next/image";
-import { useState } from "react";
+import Link from "next/link";
 import styles from "./confirmation.module.scss"; // Import the CSS module
 
 export default function ConfirmationPage() {
-  const [isLoading, setIsLoading] = useState(false);
-
-  async function onSubmit(event) {
-    event.preventDefault();
-    setIsLoading(true);
-
-    try {
-      const formData = new FormData(event.currentTarget);
-      const response = await fetch("/api/submit", {
-        method: "POST",
-        body: formData,
-      });
-
-      const data = await response.json();
-      // Handle response if necessary
-    } catch (error) {
-      // Handle error if necessary
-      console.error(error);
-    } finally {
-      setIsLoading(false);
-    }
-  }
-
   return (
     <div className={styles["confirmation-container"]}>
       <div className={styles["messageContainer"]}>
         <div className={styles.title}>Thank You!</div>
-        <div className={styles["message"]}>
-          <div className={styles.confirmationImage}>
-            <Image
-              src="/confirmIcon.jpg"
-              alt="confirmation icon"
-              width={500}
-              height={500}
-              layout="responsive"
-            />
-          </div>
-          <div className={styles.confirmationText}>
-            Your registration for the lucky draw is received.
-          </div>
-          <div className={styles.confirmationText}>Wish you best of luck!</div>
-        </div>
+        {
+          (new Date().getHours() <= 18) ? (
+            <>
+              <div className={styles["message"]}>
+                <div className={styles.confirmationImage}>
+                  <Image
+                    src="/confirmIcon.jpg"
+                    alt="confirmation icon"
+                    width={500}
+                    height={500}
+                    layout="responsive"
+                  />
+                </div>
+                <div className={styles.confirmationText}>
+                  Your registration for the lucky draw is received.
+                </div>
+                <div className={styles.confirmationText}>Wish you best of luck!</div>
+              </div>
+            </>
+          ) : (
+            <>
+              <div className={styles["message"]}>
+                <div className={styles.confirmationImage}>
+                  <Image
+                    src="/registrationOverIcon.png"
+                    alt="registration over icon"
+                    width={500}
+                    height={500}
+                    layout="responsive"
+                  />
+                </div>
+                <div className={styles.confirmationText}>
+                  Registration for the lucky draw is closed.
+                </div>
+                <div className={styles.confirmationText}>But, Do not be disappointed! esiKidz has a complimentary free 2 months trial subscription. </div>
+                <div className={styles.confirmationText}>esiKidz representative will contact soon to set up a free trial subscription after the conference.</div>
+              </div>
+            </>
+          )
+        }
         <Link className={styles["form-link"]} href="/aiSimulator">
           <div className={styles["buttonIcon"]}>
             <Image
@@ -65,9 +66,9 @@ export default function ConfirmationPage() {
       <div className={styles["form-footer"]}>
         <span>Welcome to the 2024 SECA Conference!</span>
         <span>Experience esikBot like never before.</span>
-        <a target="_blank" href="https://www.esikidz.com/">
+        <a target="_blank" href="https://www.esiKidz.com/">
           <Image
-            src="/esikidz-logo.svg"
+            src="/esiKidz-logo.svg"
             alt="enter button icon"
             width={150}
             height={70}
